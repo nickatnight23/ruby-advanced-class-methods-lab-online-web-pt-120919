80c13bf5f3c3bfc9d6beeef1b4a13c0c0631e14b
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-10 04:51:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+def initialize (name)
+  @name = name
+  @@all = self
+  
   def self.all
     @@all
   end
@@ -9,5 +13,12 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def song.find_or_create_by_name
+    @@all
 
 end
